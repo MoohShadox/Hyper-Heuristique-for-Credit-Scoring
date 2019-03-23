c72750ee7f7cbf4c21966c1414db0bac2690c220
MoohShadox <ouaguenouni.hachemi@gmail.com>
2019-03-23 22:20:56 +0100

    Métaheuristique Fonctionnelle

--- Destiny/Tresholding.py
+++ Destiny/Tresholding.py
@@ -17,9 +17,9 @@ class Tresholding:
         self.__valeurs_classe = {}
         self.__treshold_percentage = {}
         self.__proportions_classes = {}
-        self.__discriminants_ficher = {}
-        self.__volume_overlap_region = {}
-        self.__pourcentage_dehors_overlap = {}
+        self.__discriminants_ficher = None
+        self.__volume_overlap_region = None
+        self.__pourcentage_dehors_overlap = None
         self.__nb_features = 0
         self.__alpha = 0.85
 
@@ -66,29 +66,31 @@ class Tresholding:
             self.__proportions_classes[j] = self.__proportions_classes[j] / sum
 
 
-    def F1(self):
+    def F1(self,attributs):
         nb_features = len(self.__data[0])
-        for i in range(0,nb_features):
-            results = {}
-            for j in self.__valeurs_classe:
-                results[j] = {}
-                L = []
-                for k in self.__valeurs_classe[j]:
-                    L.append(k[i])
-                N = np.array(L)
-                results[j]["M"] = N.mean()
-                results[j]["V"] = N.var()
-            numerator = 0
-            for j in results:
-                for k in results:
-                    if(i!=j):
-                        numerator = numerator + self.__proportions_classes[k] * self.__proportions_classes[j] * (results[j]["M"] - results[k]["M"])*(results[j]["M"] - results[k]["M"])
-            denominateur = 0
-            for j in results:
-                denominateur = denominateur + results[j]['V'] * self.__proportions_classes[j]
-            self.__discriminants_ficher[i] = numerator / denominateur
+        if(self.__discriminants_ficher == None):
+            self.__discriminants_ficher = {}
+            for i in range(0,nb_features):
+                results = {}
+                for j in self.__valeurs_classe:
+                    results[j] = {}
+                    L = []
+                    for k in self.__valeurs_classe[j]:
+                        L.append(k[i])
+                    N = np.array(L)
+                    results[j]["M"] = N.mean()
+                    results[j]["V"] = N.var()
+                numerator = 0
+                for j in results:
+                    for k in results:
+                        if(i!=j):
+                            numerator = numerator + self.__proportions_classes[k] * self.__proportions_classes[j] * (results[j]["M"] - results[k]["M"])*(results[j]["M"] - results[k]["M"])
+                denominateur = 0
+                for j in results:
+                    denominateur = denominateur + results[j]['V'] * self.__proportions_classes[j]
+                self.__discriminants_ficher[i] = numerator / denominateur
         m = 0
-        for i in self.__discriminants_ficher:
+        for i in attributs:
             if(self.__discriminants_ficher[i]>m):
                 m = self.__discriminants_ficher[i]
         return m
@@ -116,39 +118,45 @@ class Tresholding:
     def MAXMAX(self,feature,classe1,classe2):
         return max(self.MaxF(feature,classe1),self.MaxF(feature,classe2))
 
-    def F2(self):
-        L = list(self.__valeurs_classe.keys())
-        C = combinations(L,2)
-        for i in C:
+    def F2(self,attributs):
+        if(self.__volume_overlap_region == None):
+            self.__volume_overlap_region = {}
+            L = list(self.__valeurs_classe.keys())
             produit = 1
-            for j in range(0,len(self.__data[0])):
-                A = self.MINMAX(j,i[0],i[1]) - self.MAXMIN(j,i[0],i[1])
-                A = max(0,A)
-                B = self.MAXMAX(j,i[0],i[1]) - self.MINMIN(j,i[0],i[1])
-                produit = produit * A/B
-            self.__volume_overlap_region[i] = produit
+            for j in range (0 , len (self.__data[0])):
+                C = combinations(L,2)
+                for i in C:
+                    A = self.MINMAX(j,i[0],i[1]) - self.MAXMIN(j,i[0],i[1])
+                    A = max(0,A)
+                    B = self.MAXMAX(j,i[0],i[1]) - self.MINMIN(j,i[0],i[1])
+                    produit = produit * A/B
+                    self.__volume_overlap_region[j] = produit
         p = 1
-        for i in self.__volume_overlap_region.items():
-            p = p * i[1]
+        for i in attributs:
+            p = p * self.__volume_overlap_region[i]
         return p
 
 
-    def F3(self):
-        L = list (self.__valeurs_classe.keys ())
-        s = len(self.__data)
+    def F3(self,attributs):
+        if(self.__pourcentage_dehors_overlap == None):
+            self.__pourcentage_dehors_overlap = {}
+            L = list (self.__valeurs_classe.keys ())
+            s = len(self.__data)
+            for j in range (0 , len (self.__data[0])):
+                cpt = 0
+                d = self.__data.transpose()[j]
+                C = combinations (L , 2)
+                for i in C:
+                    MMA = self.MINMAX(j,i[0],i[1])
+                    MMI = self.MAXMIN(j,i[0],i[1])
+                    for k in d:
+                        if(k<=MMA and k>=MMI):
+                            cpt = cpt + 1
+                self.__pourcentage_dehors_overlap[j] = cpt / s
         m = 0
-        for j in range (0 , len (self.__data[0])):
-            cpt = 0
-            d = self.__data.transpose()[j]
-            C = combinations (L , 2)
-            for i in C:
-                MMA = self.MINMAX(j,i[0],i[1])
-                MMI = self.MAXMIN(j,i[0],i[1])
-                for k in d:
-                    if(k<=MMA and k>=MMI):
-                        cpt = cpt + 1
-            if(cpt/s>m):
-                m = cpt/s
+        for i in attributs:
+            if(self.__pourcentage_dehors_overlap[i]>m):
+                m = self.__pourcentage_dehors_overlap[i]
             if(m==1):
                 break
         return m
@@ -158,32 +166,141 @@ class Tresholding:
     def setThresholdinDestiny(self,D,data,target):
         pass
 
+    def Energie(self,L):
+        e = self.__alpha * ((1 / self.F1 (L)) + self.F2 (L) + (1 / self.F3 (L))) / 3 + (1 - self.__alpha) * (
+                    len (L) / self.__nb_features)
+        return e
+
+    def GenererListeRandom(self):
+        r = random.randint (1 , self.__nb_features - 1)
+        L = r * [-1]
+        for i in range(0,r):
+            k = random.randint(0,self.__nb_features-1)
+            while(k in L):
+                k = random.randint (0 , self.__nb_features-1)
+            L[i] = k
+        return L
+
+    def Alteration_Diversification(self,L):
+        i = random.randint(1,self.__nb_features-1)
+        NL = i*[0]
+        print("L = ",L)
+        for j in range(0,i):
+            if(j < len(L)):
+                NL[j] = L[j]
+            else:
+                k = random.randint(0,self.__nb_features-1)
+                while(k in NL):
+                    k = random.randint(0,self.__nb_features-1)
+                NL[j] = k
+        print("NL = ", NL)
+        return NL
+
+
+    def Alteration_Insensification(self,L,T):
+        ELMIN = 0
+        ELMAX = 0
+        augmentation_taille = int(math.log(self.__nb_features,2)*T) + 1
+        if(augmentation_taille == 0):
+            augmentation_taille = 1
+        nouvelle_taille_sup = len(L) + augmentation_taille
+        nouvelle_taille_inf = len(L) - augmentation_taille
+        NLP,NLM = None,None
+        if(nouvelle_taille_inf > 0):
+            NLM = nouvelle_taille_inf*[0]
+            for w in range(0,nouvelle_taille_inf):
+                NLM[w] = L[w]
+        if(nouvelle_taille_sup <= self.__nb_features):
+            NLP = nouvelle_taille_sup*[0]
+            i = 0
+            while(i<len(L)):
+                NLP[i] = L[i]
+                i = i + 1
+            while(i<nouvelle_taille_sup):
+                z = random.randint(0,self.__nb_features-1)
+                while(z in NLP):
+                    z = random.randint (0 , self.__nb_features - 1)
+                NLP[i] = z
+                i = i + 1
+        if(NLP == None):
+            EMAX = -1
+        else:
+            EMAX = self.Energie(NLP)
+        if(NLM == None):
+            EMIN = -1
+        else:
+            EMIN = self.Energie(NLM)
+        if(EMIN > EMAX):
+            return NLM
+        else:
+            return NLP
+
+
     def getTreshold(self,data,target):
         self.__nb_features = len(data[0])
         self.fit(data,target)
-        L = list(range(0 , self.__nb_features))
-        t = 0
-        em = 1000
-        ez = 1100
-        for i in range(1,self.__nb_features , int(math.log(self.__nb_features,2))):
-            C = list(combinations(L,i))
-            for j in C:
-                print(j)
-                self.masquer(j)
-                e = self.__alpha * ((1/self.F1())) + (1-self.__alpha) * (i/self.__nb_features)
-                if(ez==e):
-                    self.fit (data , target)
+        L = self.GenererListeRandom()
+        e = self.Energie(L)
+        pdivers = 0.5
+        T = 1
+        emin,percentagemin = 1000,1000
+        nb_iterations_pas = 10
+        cptcpt = nb_iterations_pas
+        ep = 0
+        pas = 0.20
+        coef_decrementation_temperature = 0.80
+        while(T  > 0.01):
+            k = random.randint(1,100)
+            #Mecanisme de régulation des diversification / intensification
+            if(k<pdivers*100):
+                NL = self.Alteration_Diversification(L)
+            else:
+                NL = self.Alteration_Insensification(L,T)
+            print("Le nouvel ensemble a estimer est ",NL)
+            #Mise a jour de la température
+            if(e == ep):
+                T = T*(1+coef_decrementation_temperature/10)
+            T = T * coef_decrementation_temperature
+            if(cptcpt == 0):
+                cptcpt = nb_iterations_pas
+                T = T - pas
+            print("La température est : ",T)
+            #Mise a jour de l'energie
+            ep = e
+            enew = self.Energie(NL)
+
+            if(enew<=e):
+                L = NL
+                e = enew
+            else:
+                try:
+                    p = math.exp (-((enew - e) / T))
+                except(OverflowError):
                     break
-                ez = e
-                if(e<em):
-                    em = e
-                    t = i/self.__nb_features
-                print(em)
-                self.fit (data , target)
-        return t
+                p = int(p*100)
+                rr = random.randint(0,100)
+                if(p<rr):
+                    L = NL
+                    e = enew
+            print("Et donc la nouvelle energie s'èleve a ",e)
+            #Concerver le maximum
+            if(e<emin):
+                emin = e
+                percentagemin = len(L) / self.__nb_features
+            #Condition d'arret
+            if(T < 0.01):
+                break
+        return emin,percentagemin
 
 
 
 
+
+
+
+data,target = load_musk_dataset()
+T = Tresholding()
+T.fit(data,target)
+print(T.getTreshold(data,target))
 
 
