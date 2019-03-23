cd6f0a2f25b295f56c51a17c48e431c08666edb8
MoohShadox <ouaguenouni.hachemi@gmail.com>
2019-03-23 07:16:47 +0100

    Rectification Thresholding 1

--- Destiny/RankingFunctions/Test.py
+++ Destiny/RankingFunctions/Test.py
@@ -1,19 +1,12 @@
-from sklearn.datasets import make_classification
 
-from Destiny.DataSets import german_dataset
-from Destiny.DataSets.australian_dataset import load_australian_dataset
-from Destiny.DataSets.load_spambase_dataset import load_spambase_dataset
 from Destiny.Destin import Destiny
-from Destiny.RankingFunctions import ReliefF
-from Destiny.RankingFunctions.Final.ChiMesure import chi
-from Destiny.RankingFunctions.Final.FScore import FScore
+
 from Nature2.Nature import Nature
-from sklearn.datasets import load_wine
-import pandas as pd
-from Destiny.DataSets import load_kaggle_dataset
-#data, target = german_dataset.load_german_dataset()
-#data,target = wine.data, wine.target
-data,target = load_australian_dataset()
+
+from Destiny.DataSets.madelon_dataset import load__train_dataset
+
+data,target = load__train_dataset()
+target = target.transpose()
 print("dumped")
 DM= Destiny()
 DM.fit(data,target)
